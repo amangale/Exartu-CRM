8aee2cbb86282a4918e3f741f370a636ba896076
Mihai Constantinescu <constantinescu_mihai@ymail.com>
2014-01-25 19:51:40 +0200

    front-end

---
+++ front-end/js/exartu.dashboard.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+	
+
+	// === Prepare sparklines charts === //
+	exartu.sparkline();
+  
+
+});
+
+
+exartu = {
+		// === Peity charts === //
+		sparkline: function(){		
+			$(".sparkline_line_good span").sparkline("html", {
+				type: "line",
+				fillColor: "#4cd964",
+				lineColor: "#4cd964",
+				width: "50",
+				height: "24"
+			});
+			$(".sparkline_line_bad span").sparkline("html", {
+				type: "line",
+				fillColor: "#de0a0a",
+				lineColor: "#de0a0a",
+				width: "50",
+				height: "24"
+			});	
+			$(".sparkline_line_neutral span").sparkline("html", {
+				type: "line",
+				fillColor: "#CCCCCC",
+				lineColor: "#757575",
+				width: "50",
+				height: "24"
+			});
+			
+			$(".sparkline_bar_good span").sparkline('html',{
+				type: "bar",
+				barColor: "#4cd964",
+				barWidth: "5",
+				height: "24"
+			});
+			$(".sparkline_bar_bad span").sparkline('html',{
+				type: "bar",
+				barColor: "#de0a0a",
+				barWidth: "5",
+				height: "24"
+			});	
+			$(".sparkline_bar_neutral span").sparkline('html',{
+				type: "bar",
+				barColor: "#757575",
+				barWidth: "5",
+				height: "24"
+			});
+		},
+
+		// === Tooltip for flot charts === //
+		flot_tooltip: function(x, y, contents) {
+			
+			$('<div id="tooltip">' + contents + '</div>').css( {
+				top: y + 5,
+				left: x + 5
+			}).appendTo("body").fadeIn(200);
+		}
+}
